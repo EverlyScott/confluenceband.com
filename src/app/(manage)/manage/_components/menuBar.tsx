dcd432f768197c5a9528fa52915dcc014c71aa0f
Everly Scott <everly@everly.page>
2026-04-20 22:48:26 -0500

    Add home btn on manage page

--- src/app/(manage)/manage/_components/menuBar.tsx
+++ src/app/(manage)/manage/_components/menuBar.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  Link,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import Home from "@mui/icons-material/Home";
 
 const MenuBar: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Manage
-          </Typography>
+          <Tooltip title="Home">
+            <IconButton href="/">
+              <Home />
+            </IconButton>
+          </Tooltip>
+          <div style={{ flexGrow: 1, marginLeft: "1rem", display: "flex" }}>
+            <Link underline="hover" color="inherit" href="/manage">
+              <Typography variant="h6" component="div">
+                Manage
+              </Typography>
+            </Link>
+          </div>
           <UserButton />
         </Toolbar>
       </AppBar>
